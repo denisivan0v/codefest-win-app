5a49a21dc90f42f0e722183fd6c12d2ffe43c72d
denisivan0v <ivanoff.denis@gmail.com>
2014-11-19 10:52:51 +0600

    lost file

---
+++ CodeFestApp/CodeFestApp/CodeFestApp.Windows/ViewModels/SectionViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CodeFestApp.Data;
+
+using ReactiveUI;
+
+namespace CodeFestApp.ViewModels
+{
+    public class SectionViewModel : ReactiveObject, IRoutableViewModel
+    {
+        private readonly SampleDataGroup _sampleDataGroup;
+
+        public SectionViewModel(IScreen hostScreen, SampleDataGroup sampleDataGroup)
+        {
+            _sampleDataGroup = sampleDataGroup;
+            HostScreen = hostScreen;
+        }
+
+        public string UrlPathSegment
+        {
+            get { return "section"; }
+        }
+
+        public IScreen HostScreen { get; private set; }
+    }
+}
