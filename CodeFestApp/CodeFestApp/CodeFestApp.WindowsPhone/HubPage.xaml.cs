eeb16db8790f085141ab64bb1fcd7aadea10f6ca
denisivan0v <ivanoff.denis@gmail.com>
2015-01-20 00:30:19 +0600

    reverting binding experiments

--- CodeFestApp/CodeFestApp/CodeFestApp.WindowsPhone/HubPage.xaml.cs
+++ CodeFestApp/CodeFestApp/CodeFestApp.WindowsPhone/HubPage.xaml.cs
@@ -33,10 +33,7 @@ namespace CodeFestApp
             DisplayInformation.AutoRotationPreferences = DisplayOrientations.Portrait;
             NavigationCacheMode = NavigationCacheMode.Required;
 
-            this.Bind(ViewModel, x => x.Groups, x => x.Section1.DataContext);
-            this.Bind(ViewModel, x => x.Groups[0], x => x.Section2.DataContext);
-            this.Bind(ViewModel, x => x.Groups[1], x => x.Section3.DataContext);
-            this.Bind(ViewModel, x => x.Groups[2], x => x.Section4.DataContext);
+            this.Bind(ViewModel, x => x.Groups, x => x.Hub.DataContext);
 
             /*
             var sections = FindChildControl<ListView>(Root, "Sections") as ListView;
@@ -175,30 +172,5 @@ namespace CodeFestApp
 
         public HubViewModel ViewModel { get; set; }
 
-        private DependencyObject FindChildControl<T>(DependencyObject control, string ctrlName)
-        {
-            int childNumber = VisualTreeHelper.GetChildrenCount(control);
-            for (int i = 0; i < childNumber; i++)
-            {
-                DependencyObject child = VisualTreeHelper.GetChild(control, i);
-                FrameworkElement fe = child as FrameworkElement;
-                // Not a framework element or is null
-                if (fe == null) return null;
-
-                if (child is T && fe.Name == ctrlName)
-                {
-                    // Found the control so return
-                    return child;
-                }
-                else
-                {
-                    // Not found it - search children
-                    DependencyObject nextLevel = FindChildControl<T>(child, ctrlName);
-                    if (nextLevel != null)
-                        return nextLevel;
-                }
-            }
-            return null;
-        }
     }
 }
