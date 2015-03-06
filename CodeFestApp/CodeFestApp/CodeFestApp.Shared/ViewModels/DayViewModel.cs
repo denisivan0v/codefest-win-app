46ce833e4804795f27b7ff1bcced4e9dce1a4666
denisivan0v <ivanoff.denis@gmail.com>
2015-03-07 00:26:50 +0600

    minor changes

--- CodeFestApp/CodeFestApp/CodeFestApp.Shared/ViewModels/DayViewModel.cs
+++ CodeFestApp/CodeFestApp/CodeFestApp.Shared/ViewModels/DayViewModel.cs
@@ -11,14 +11,22 @@ namespace CodeFestApp.ViewModels
 {
     public class DayViewModel : ReactiveObject, IRoutableViewModel
     {
-        private readonly IViewModelFactory _lectureViewModelFactory;
         private readonly Day _day;
+
+        private readonly IEnumerable<IGrouping<string, LectureViewModel>> _lectures;
+        private readonly ReactiveList<Uri> _speakerAvatars;
 
         public DayViewModel(IScreen hostScreen, Day day, IViewModelFactory lectureViewModelFactory)
         {
             HostScreen = hostScreen;
             _day = day;
-            _lectureViewModelFactory = lectureViewModelFactory;
+
+            _lectures = _day.Lectures
+                            .Select(lectureViewModelFactory.Create<LectureViewModel, Lecture>)
+                            .OrderBy(x => x.Start)
+                            .GroupBy(x => x.Start.ToString("t"));
+
+            _speakerAvatars = new ReactiveList<Uri>(_day.Lectures.SelectMany(x => x.Speakers).Select(x => x.Avatar));
 
 
             NavigateToLectureCommand = ReactiveCommand.Create();
@@ -37,15 +45,18 @@ namespace CodeFestApp.ViewModels
             get { return _day.Date; }
         }
 
-        public IEnumerable<IGrouping<string, LectureViewModel>> Lectures
+        public ReactiveList<Uri> SpeakerAvatars
         {
             get
             {
-                return _day.Lectures
-                           .Select(x => _lectureViewModelFactory.Create<LectureViewModel, Lecture>(x))
-                           .OrderBy(x => x.Start)
-                           .GroupBy(x => x.Start.ToString("t"));
+                _speakerAvatars.Sort((x, y) => (new Random()).Next(0, 10));
+                return _speakerAvatars;
             }
+        }
+
+        public IEnumerable<IGrouping<string, LectureViewModel>> Lectures
+        {
+            get { return _lectures; }
         }
 
         public ReactiveCommand<object> NavigateToLectureCommand { get; private set; }  
